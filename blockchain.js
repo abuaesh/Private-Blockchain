7ff4811834272d75e6300903b0620193f5ae5649
abuaesh <52622393+abuaesh@users.noreply.github.com>
2019-08-04 13:22:51 +0300

    Add files via upload

--- blockchain.js
+++ blockchain.js
@@ -10,7 +10,9 @@
 
 const SHA256 = require('crypto-js/sha256');
 const BlockClass = require('./block.js');
+const bitcoin = require('bitcoinjs-lib'); // v3.x.x
 const bitcoinMessage = require('bitcoinjs-message');
+
 
 class Blockchain {
 
@@ -107,16 +109,18 @@ class Blockchain {
     requestMessageOwnershipVerification(address) {
         return new Promise((resolve) => {
             //resolve(address:${new Date().getTime().toString().slice(0, -3)}:starRegistry;);
-            var keyPair = bitcoinMessage.ECPair.fromWIF(address); //address = '5KYZdUEo39z3FPrtuX2QbbwGnNP5zTd7yyr2SC1j299sBCnWjss'
+            var keyPair = bitcoin.ECPair.fromWIF(address); //'5KYZdUEo39z3FPrtuX2QbbwGnNP5zTd7yyr2SC1j299sBCnWjss'
             var privateKey = keyPair.privateKey;
+            var publicKey = keyPair.publicKey;
+            console.log('The private key is: ' + JSON.stringify(privateKey));
+            console.log('The public key is: ' + JSON.stringify(publicKey));
+
             var message = 'This is an example of a signed message.';
 
             var signature = bitcoinMessage.sign(message, privateKey, keyPair.compressed);
             console.log(signature.toString('base64'));
             // => 'G9L5yLFjti0QTHhPyFrZCT1V/MMnBtXKmoiKDZ78NDBjERki6ZTQZdSMCtkgoNmp17By9ItJr8o7ChX0XxY91nk='
-
-            resolve(bitcoinMessage.verify(message, address, signature)); //=> true
-
+            resolve(bitcoinMessage.verify(message, '5KYZdUEo39z3FPrtuX2QbbwGnNP5zTd7yyr2SC1j299sBCnWjss', signature)); //=> true
         });
     }
 
@@ -228,7 +232,7 @@ class Blockchain {
 module.exports.Blockchain = Blockchain;
 
     let bc = new Blockchain();
-
+/*
 
     let b1 = new BlockClass.Block('Block 1');
     bc._addBlock(b1);
@@ -240,3 +244,6 @@ module.exports.Blockchain = Blockchain;
     bc._addBlock(b3);
 
     //bc.submitStar('87987676e8789f8766564d769708c09', 'Hi there!', '68655635e5890e98098796a8769709');
+*/
+
+bc.requestMessageOwnershipVerification('5KYZdUEo39z3FPrtuX2QbbwGnNP5zTd7yyr2SC1j299sBCnWjss').then(msg => console.log(msg));
